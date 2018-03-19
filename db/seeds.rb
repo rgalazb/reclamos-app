63a21687b5ebf34d35f629bb0b3c66cfbe008f34
rgalazb <galaz.becerra@gmail.com>
2018-03-19 12:49:55 -0300

    seed completo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Company.destroy_all
+User.destroy_all
+Complain.destroy_all
+
+20.times do
+  Company.create(name: Faker::Company.name)
+end
+
+30.times do
+  User.create(email: "#{Faker::Pokemon.name}@gmail.com", password:'123123')
+end
+
+250.times do
+  Complain.create(user_id: User.all.sample.id, company_id: Company.all.sample.id, content: "#{Faker::RickAndMorty.quote}")
+end
